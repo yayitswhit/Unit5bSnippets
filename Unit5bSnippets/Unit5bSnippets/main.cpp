34f074625a5a3eeda507dcac878d83b71ae80a12
yayitswhit <104163475+yayitswhit@users.noreply.github.com>
2023-03-15 14:49:35 -0400

    first draft

--- Unit5bSnippets/Unit5bSnippets/main.cpp
+++ Unit5bSnippets/Unit5bSnippets/main.cpp
@@ -17,6 +17,7 @@ int main() {
     }
     
     //nested for loop
+    //will do 10 instances of the inner tabbed loop in between each iteration of the outer loop. The inner loop will execute 10 times per each outer loop iteration.
     for(int i = 1; i < 11; i++) {
         cout << "Outer Loop Counter: " << i << endl;
         for(int j = 1; j < 11; j++) {
